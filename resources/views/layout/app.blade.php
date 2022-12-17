c42ca0738582b937adb0931e00c36a78e07c7dc4
Mariosam2 <marco.mariosaa@gmail.com>
2022-12-17 23:01:51 +0100

    added favicon

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="{{Vite::asset('resources/images/favicon.ico')}}" type="image/x-icon">
 
     <title>DC Comics</title>
 
